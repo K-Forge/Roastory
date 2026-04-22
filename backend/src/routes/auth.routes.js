32cbd0e303ef2925729ca5b86b633bd3456c4640
13rianVargas <bsvargasc13@gmail.com>
2026-04-22 14:25:59 -0500

    feat: apply verifyToken middleware to protect /api/auth/me route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/auth.controller');
+const { register, login, getProfile } = require('../controllers/auth.controller');
+const { verifyToken } = require('../middlewares/auth.middleware');
 
 // @route   POST /api/auth/register
 // @desc    Register a new user and hash password with Bcrypt
@@ -11,5 +12,10 @@ router.post('/register', register);
 // @desc    Authenticate user & return JWT token
 // @access  Public
 router.post('/login', login);
+
+// @route   GET /api/auth/me
+// @desc    Get user profile (Protected route)
+// @access  Private
+router.get('/me', verifyToken, getProfile);
 
 module.exports = router;
