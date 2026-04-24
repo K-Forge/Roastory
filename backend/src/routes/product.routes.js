af1d9f0c255fcb168155d9b58cd9cca8905fdf91
13rianVargas <bsvargasc13@gmail.com>
2026-04-24 10:24:00 -0500

    feat: protect product write routes with jwt and role checks

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -8,17 +8,18 @@ const {
   deleteProduct,
   updateStock
 } = require('../controllers/product.controller');
+const { verifyToken, requireRole } = require('../middlewares/auth.middleware');
 
-router.post('/', createProduct);
+router.post('/', verifyToken, requireRole(['ADMIN', 'INVENTORY_MANAGER']), createProduct);
 
 router.get('/', getProducts);
 
 router.get('/:id', getProductById);
 
-router.put('/:id', updateProduct);
+router.put('/:id', verifyToken, requireRole(['ADMIN', 'INVENTORY_MANAGER']), updateProduct);
 
-router.patch('/:id/stock', updateStock);
+router.patch('/:id/stock', verifyToken, requireRole(['ADMIN', 'INVENTORY_MANAGER']), updateStock);
 
-router.delete('/:id', deleteProduct);
+router.delete('/:id', verifyToken, requireRole(['ADMIN', 'INVENTORY_MANAGER']), deleteProduct);
 
 module.exports = router;
