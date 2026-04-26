d7cb81c73ca87e14c3e0b4cc8a217451c9df11a9
Sebastian Angulo <sebasanguloc@gmail.com>
2026-04-25 19:43:51 -0500

    feat: endpoint para obtener facturas de usuario autenticado

--- backend/src/routes/invoice.routes.js
+++ backend/src/routes/invoice.routes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const {
   createInvoice,
   getInvoices,
+  getMyInvoices,
 } = require('../controllers/invoice.controller');
 const { verifyToken, requireRole } = require('../middlewares/auth.middleware');
 
 router.post('/', verifyToken, requireRole(['ADMIN', 'CASHIER']), createInvoice);
 
 router.get('/', verifyToken, requireRole(['ADMIN']), getInvoices);
+
+router.get('/me', verifyToken, getMyInvoices);
 
 
 module.exports = router;
